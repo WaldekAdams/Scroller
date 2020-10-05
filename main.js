306faa9bf09ce4e8f98e5b19aa9e474b00955ade
Waldemar Adams <waldek-adamski@wp.pl>
2020-10-05 07:59:29 +0200

    new fn: add listener on document

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const root = document.querySelector('.root');
 const sections = root.querySelectorAll('section');
 let currentSectionIndex = 0;
+
+document.addEventListener('mousewheel', function (e) {
+    console.log(e.wheelDelta);
+    const direction = e.wheelDelta < 0 ? -1 : 1;
+})

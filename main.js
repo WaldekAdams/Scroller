bb39adb4c27e3cf0a2e0eb67be91d35f05e043f3
Waldemar Adams <waldek-adamski@wp.pl>
2020-10-12 21:32:02 +0200

    delate previous fn in main.js

--- main.js
+++ main.js
@@ -6,27 +6,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.addEventListener('mousewheel', scroller.listenScroll)
 
-
-
-    function scrollToCurentSection() {
-        sections[currentSectionIndex].scrollIntoView({
-            behavior: 'smooth',
-            block: "start",
-        })
-    }
-
-    function scrolling(direction) {
-        if (direction === 1) {
-            const isLastSection = currentSectionIndex === sections.length - 1;
-            if (isLastSection) return;
-        } else if (direction === -1) {
-            const firstSection = currentSectionIndex === 0;
-            if (firstSection) return;
-        }
-
-        currentSectionIndex = currentSectionIndex + direction;
-        // console.log(currentSectionIndex);
-
-        scrollToCurentSection()
-    }
 })

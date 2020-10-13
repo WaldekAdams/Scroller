78bbe6a222dc1fe742b090657b5fcd50d313aff9
Waldemar Adams <waldek-adamski@wp.pl>
2020-10-13 08:16:25 +0200

    new method-find out which section is in view

--- Scroller.js
+++ Scroller.js
@@ -42,4 +42,14 @@ class Scroller {
             block: "start",
         })
     }
+
+    isScrolledIntoView(el) {
+        const rect = el.getBoundingClientRect();
+        const elemTop = rect.top;
+        const elemBottom = Math.floor(rect.bottom);
+
+        const isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+
+        return isVisible;
+    }
 }
